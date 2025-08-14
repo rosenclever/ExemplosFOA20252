b12c091c1ff029ddda453a1f064302f6252b783f
rosenclever <rosenclever@gmail.com>
2025-08-13 21:09:06 -0300

    enunciado do exercício IRPF

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -4,4 +4,13 @@ Console.Write("Digite seu nome: ");
 nome = Console.ReadLine();
 Console.Write("Digite sua idade: ");
 idade = Convert.ToByte(Console.ReadLine());
-Console.WriteLine("Olá " + nome + ". A idade informada foi {0}", idade);
+Console.WriteLine("Olá " + nome + ". A idade informada foi {0}", idade);// idade = 20
+idade *= 2; // idade = idade * 2 
+Console.WriteLine($"A nova idade é {idade++}"); //40
+Console.WriteLine($"A nova idade é {++idade}"); //42
+
+//altere o programa para que solicite ao usuário informar o seu salário e ao final informe o desconto de IRPF de acordo com o salário informado. Utilize a tabela oficial de descontos de IRPF do Brasil.
+
+
+
+
