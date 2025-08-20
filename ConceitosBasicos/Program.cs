6046346992ea5ccd35c6809214c3a2751534043b
rosenclever <rosenclever@gmail.com>
2025-08-20 20:05:03 -0300

    IRPF funcionanndo

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -1,4 +1,5 @@
-﻿byte idade;
+﻿/*
+byte idade;
 string nome;
 Console.Write("Digite seu nome: ");
 nome = Console.ReadLine();
@@ -8,9 +9,25 @@ Console.WriteLine("Olá " + nome + ". A idade informada foi {0}", idade);// idad
 idade *= 2; // idade = idade * 2 
 Console.WriteLine($"A nova idade é {idade++}"); //40
 Console.WriteLine($"A nova idade é {++idade}"); //42
+*/
 
 //altere o programa para que solicite ao usuário informar o seu salário e ao final informe o desconto de IRPF de acordo com o salário informado. Utilize a tabela oficial de descontos de IRPF do Brasil.
 
+decimal salario, irpfDevido;
+Console.Write("Informe o salário: ");
+salario = decimal.Parse(Console.ReadLine());
 
+if (salario <= 2428.8m)
+    irpfDevido = 0;
+else if (salario <= 2826.65m)
+    irpfDevido = salario * 0.075m - 182.16m;
+else if (salario <= 3751.05m)
+    irpfDevido = salario * 0.15m - 394.16m;
+else if (salario <= 4664.68m)
+    irpfDevido = salario * 0.225m - 675.49m;
+else
+    irpfDevido = salario * 0.275m - 908.73m;
+
+Console.WriteLine($"Para o salario {salario:C2} o IRPF a ser pago é {irpfDevido:C2}");
 
 
