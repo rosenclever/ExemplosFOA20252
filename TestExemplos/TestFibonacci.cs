15722a832ad43acb1a2cda597a032b96af900955
rosenclever <rosenclever@gmail.com>
2025-10-01 20:02:11 -0300

    teste Fibonacci implementado, faltando algoritmo de fibonacci

---
+++ TestExemplos/TestFibonacci.cs
@@ -0,0 +1,29 @@
+﻿using ConceitosBasicos.uteis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestExemplos
+{
+    [TestClass]
+    public class TestFibonacci
+    {
+        [TestMethod]
+        public void TestValorEmPosicaoDaSequenciaDeFibonacci()
+        {
+            //cenario
+            int posicao = 6;
+            int valorEsperado = 8;
+            int valorObtido;
+            Fibonacci fibonacci = new Fibonacci();
+
+            //acao
+            valorObtido = fibonacci.ValorNaPosicao(posicao);
+
+            //verificação
+            Assert.AreEqual(valorEsperado, valorObtido);
+        }
+    }
+}
